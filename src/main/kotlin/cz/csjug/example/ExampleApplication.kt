1250df85e8d20d527c77ed83c5e1a7d2b3e8433b
Filip Kucera (cen80113) <fikucera@csas.cz>
2019-06-28 15:16:33 +0200

    init

---
+++ src/main/kotlin/cz/csjug/example/ExampleApplication.kt
@@ -0,0 +1,11 @@
+package cz.csjug.example
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<ExampleApplication>(*args)
+}
