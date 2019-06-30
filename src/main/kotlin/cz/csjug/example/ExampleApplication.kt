07dcc5f145d6f9dbe9ad20a862392bbabc96cf74
Filip Kucera <kucerafilip@gmail.com>
2019-06-30 13:24:26 +0200

    init

--- src/main/kotlin/cz/csjug/example/ExampleApplication.kt
+++ src/main/kotlin/cz/csjug/example/ExampleApplication.kt
@@ -1,5 +1,6 @@
 package cz.csjug.example
 
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
